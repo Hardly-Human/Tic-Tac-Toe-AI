a75b9111eff98a951b374485228cac2606186f9a
Rehanuddin Shaik <rehanuddin1024@gmail.com>
2024-04-14 12:35:24 -0600

    fixed docker env issue

--- create-docker-image.sh
+++ create-docker-image.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This script needs ANTHROPIC_API_KEY & OPENAI_API_KEY as ENV vars
-docker buildx create --use;
-docker buildx inspect --bootstrap;
-docker buildx build --load  \
+docker build  \
              --build-arg ANTHROPIC_API_KEY=${ANTHROPIC_API_KEY} \
              --build-arg OPENAI_API_KEY=${OPENAI_API_KEY} \
              -t rshaik921/tic-tac-toe-ai:latest \
